a73dc41746daebe686c18747cf27f28c316e5e8d
shui91 <sherman.sy.hui@gmail.com>
2016-05-17 00:20:05 -0700

    chore: wip to retrieve api data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,25 @@
 'use strict';
-let demo = document.getElementById('location');
+let demo = document.getElementById('location'),
+    weatherData = null,
+    endpoint = '';
+
 if ('geolocation' in navigator) {
   /* geolocation is available */
   navigator.geolocation.getCurrentPosition(function(position){
-      console.log(position.coords);
-      let coordinates = position.coords;
-      demo.innerHTML = 'Your coordinates are, ' + coordinates.latitude + ', ' + coordinates.longitude;
+      let coordinates = position.coords,
+        lat = coordinates.latitude.toFixed(3),
+        long = coordinates.longitude.toFixed(3),
+        endpoint = 'api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon='+ long;
+        // endpoint = '/api.openweathermap.org/data/2.5/weather?lat={1}&lon={2}'.assign(lat, long);
+
+        console.log(endpoint);
+
+     $.getJSON(endpoint).then(function(res){
+          console.log(res);
+      });
+     //
+    //   console.log(weatherData);
+      demo.innerHTML = 'Your coordinates are,  ' + coordinates.latitude + ', ' + coordinates.longitude;
   });
 } else {
   /* geolocation IS NOT available */
