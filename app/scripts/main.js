75a9ad0b5edf54c112eae39bcea0ec0c8b8f7bdb
shui91 <sherman.sy.hui@gmail.com>
2016-05-17 10:55:23 -0700

    feat: remove tpl, refactor code to use angular service and ctrl

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,27 +1,70 @@
-'use strict';
-let demo = document.getElementById('location'),
-    weatherData = null,
-    endpoint = '';
+// 'use strict';
+// let demo = document.getElementById('location'),
+//     weatherData = null,
+//     endpoint = '';
+//
+// if ('geolocation' in navigator) {
+//   /* geolocation is available */
+//   navigator.geolocation.getCurrentPosition(function(position){
+//       let coordinates = position.coords,
+//         lat = coordinates.latitude.toFixed(3),
+//         long = coordinates.longitude.toFixed(3),
+//         endpoint = 'api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon='+ long;
+//         // endpoint = '/api.openweathermap.org/data/2.5/weather?lat={1}&lon={2}'.assign(lat, long);
+//
+//         console.log(endpoint);
+//
+//      $.getJSON(endpoint).then(function(res){
+//           console.log(res);
+//       });
+//      //
+//     //   console.log(weatherData);
+//       demo.innerHTML = 'Your coordinates are,  ' + coordinates.latitude + ', ' + coordinates.longitude;
+//   });
+// } else {
+//   /* geolocation IS NOT available */
+//   console.log('geolocation not available, please enable!');
+// }
 
-if ('geolocation' in navigator) {
-  /* geolocation is available */
-  navigator.geolocation.getCurrentPosition(function(position){
-      let coordinates = position.coords,
-        lat = coordinates.latitude.toFixed(3),
-        long = coordinates.longitude.toFixed(3),
-        endpoint = 'api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon='+ long;
-        // endpoint = '/api.openweathermap.org/data/2.5/weather?lat={1}&lon={2}'.assign(lat, long);
+(function() {
+  'use strict';
 
-        console.log(endpoint);
+  let demo = document.getElementById('location');
 
-     $.getJSON(endpoint).then(function(res){
-          console.log(res);
-      });
-     //
-    //   console.log(weatherData);
-      demo.innerHTML = 'Your coordinates are,  ' + coordinates.latitude + ', ' + coordinates.longitude;
-  });
-} else {
-  /* geolocation IS NOT available */
-  console.log('geolocation not available, please enable!');
-}
+  function weatherAPI($http, $q, endpoint) {
+    return {
+      getData: function() {
+        if ('geolocation' in navigator) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            let coordinates = position.coords,
+              lat = coordinates.latitude.toFixed(3),
+              long = coordinates.longitude.toFixed(3),
+              ep = endpoint + '?lat=' + lat + '&lon=' + long;
+
+              console.log(coordinates);
+
+            demo.innerHTML = 'Your coordinates are, ' + coordinates.latitude + ', ' + coordinates.longitude;
+          });
+        } else {
+          console.log('geolocation not available, please enable');
+        }
+      }
+    };
+  }
+
+  weatherAPI.$inject = ['$http', '$q', 'endpoint'];
+
+  function mainCtrl($scope, $q, weatherAPI, endpoint) {
+    let self = $scope.ctrl = this;
+
+    weatherAPI.getData();
+  }
+
+  mainCtrl.$inject = ['$scope', '$q', 'weatherAPI', 'endpoint'];
+
+  angular
+    .module('weatherApp', [])
+    .constant('endpoint', 'api.openweathermap.org/data/2.5/weather?')
+    .service('weatherAPI', weatherAPI)
+    .controller('mainCtrl', mainCtrl);
+})();
