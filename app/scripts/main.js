7cbeb073782658409a45724a4fa01c011914f24a
shui91 <sherman.sy.hui@gmail.com>
2016-05-16 23:11:58 -0700

    feat: receiving location from user's browser

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+let demo = document.getElementById('location');
+if ('geolocation' in navigator) {
+  /* geolocation is available */
+  navigator.geolocation.getCurrentPosition(function(position){
+      console.log(position.coords);
+      let coordinates = position.coords;
+      demo.innerHTML = 'Your coordinates are, ' + coordinates.latitude + ', ' + coordinates.longitude;
+  });
+} else {
+  /* geolocation IS NOT available */
+  console.log('geolocation not available, please enable!');
+}
