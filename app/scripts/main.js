947ae279bff2e45bd795318645d46c7f21f45b64
shui91 <sherman.sy.hui@gmail.com>
2016-05-22 23:26:40 -0700

    feat: format html clean up code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,31 +1,3 @@
-// 'use strict';
-// let demo = document.getElementById('location'),
-//     weatherData = null,
-//     endpoint = '';
-//
-// if ('geolocation' in navigator) {
-//   /* geolocation is available */
-//   navigator.geolocation.getCurrentPosition(function(position){
-//       let coordinates = position.coords,
-//         lat = coordinates.latitude.toFixed(3),
-//         long = coordinates.longitude.toFixed(3),
-//         endpoint = 'api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon='+ long;
-//         // endpoint = '/api.openweathermap.org/data/2.5/weather?lat={1}&lon={2}'.assign(lat, long);
-//
-//         console.log(endpoint);
-//
-//      $.getJSON(endpoint).then(function(res){
-//           console.log(res);
-//       });
-//      //
-//     //   console.log(weatherData);
-//       demo.innerHTML = 'Your coordinates are,  ' + coordinates.latitude + ', ' + coordinates.longitude;
-//   });
-// } else {
-//   /* geolocation IS NOT available */
-//   console.log('geolocation not available, please enable!');
-// }
-
 (function() {
   'use strict';
 
@@ -43,6 +15,8 @@
 
   function mainCtrl($scope, $q, weatherAPI, endpoint) {
     let self = $scope.ctrl = this;
+
+    self.data = null;
 
     if ('geolocation' in navigator) {
      return navigator.geolocation.getCurrentPosition(function(position) {
