7ed0f87a694e9f1f4742464382c1b2cc3c9ec24b
shui91 <sherman.sy.hui@gmail.com>
2016-05-29 16:16:24 -0700

    feat: add change temperature functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -61,29 +61,50 @@
 
     self.data = null;
 
+    //setting background image happens too early; async problem
     self.displayWeather = function(type){
         let id = document.getElementById('icon');
         if (type >= 200 && type <= 232) {
             id.className += ' wi-day-thunderstorm';
+            self.data.bgimg = 'images/thunder.jpeg';
         } else if (type >= 300 && type <= 321) {
             id.className += ' wi-day-sprinkle';
+            self.data.bgimg = 'images/sprinkle.jpeg';
         } else if (type >= 500 && type <= 531) {
             id.className += ' wi-day-rain';
+            self.data.bgimg = 'images/rain.jpeg';
         } else if (type >= 600 && type <= 622) {
             id.className += ' wi-day-snow-wind';
+            self.data.bgimg = 'images/snow.jpeg';
         } else if (type >= 701 && type <= 781) {
             id.className += ' wi-day-cloudy-windy';
-        } else if (type == 800) {
+            self.data.bgimg = 'images/cloud.jpeg';
+        } else if (type === 800) {
             id.className += ' wi-day-sunny';
+            self.data.bgimg = 'images/sun.jpeg';
         } else if (type >= 801 && type <= 804){
             id.className += ' wi-day-cloudy';
+            self.data.bgimg = 'images/cloud.jpeg';
         } else {
             alert ('either something crazy happening or nothing at all');
         }
     };
 
     self.changeUnits = function(){
+        if (self.data.tempInfo.isCelsius){
 
+            self.data.tempInfo.unit = 'F';
+            self.data.tempInfo.isCelsius = false;
+            self.data.tempInfo.temp = Math.round((self.data.tempInfo.temp * 9) / 5 + 32).toFixed(2);
+
+            return self.data.tempInfo;
+        } else {
+            self.data.tempInfo.unit = 'C';
+            self.data.tempInfo.isCelsius = true;
+            self.data.tempInfo.temp = ((self.data.tempInfo.temp - 32) * 5 / 9).toFixed(2);
+
+            return self.data.tempInfo;
+        }
     };
 
     if ('geolocation' in navigator) {
@@ -98,7 +119,11 @@
         weatherAPI.getData(url).then(function(res) {
             self.data = res.data;
             self.data.area = res.data.name + ', ' + res.data.sys.country;
-            self.data.bgimg = 'images/sun.jpeg';
+            self.data.tempInfo = {
+                unit: 'C',
+                isCelsius: true,
+                temp: res.data.main.temp
+            };
 
             self.displayWeather(self.data.weather[0].id);
 
